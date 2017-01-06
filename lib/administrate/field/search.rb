57df30dc8cf42e25faf09ff1bad140b802813581
Muneef Hameed <hey@muneef.in>
2017-01-06 15:12:51 +0530

    cloned has_many stuffs

--- lib/administrate/field/search.rb
+++ lib/administrate/field/search.rb
@@ -1,12 +1,57 @@
-require "administrate/field/base"
-require "rails"
+require_relative "associative"
+require "administrate/page/collection"
 
 module Administrate
   module Field
-    class Search < Administrate::Field::Base
-      VERSION = "0.0.2"
+    class Search < Associative
+      DEFAULT_LIMIT = 5
 
-      class Engine < ::Rails::Engine
+      def self.permitted_attribute(attribute)
+        { "#{attribute.to_s.singularize}_ids".to_sym => [] }
+      end
+
+      def associated_collection
+        Administrate::Page::Collection.new(associated_dashboard)
+      end
+
+      def attribute_key
+        permitted_attribute.keys.first
+      end
+
+      def associated_resource_options
+        candidate_resources.map do |resource|
+          [display_candidate_resource(resource), resource.id]
+        end
+      end
+
+      def selected_options
+        data && data.map(&:id)
+      end
+
+      def limit
+        options.fetch(:limit, DEFAULT_LIMIT)
+      end
+
+      def permitted_attribute
+        self.class.permitted_attribute(attribute)
+      end
+
+      def resources
+        data.limit(limit)
+      end
+
+      def more_than_limit?
+        data.count(:all) > limit
+      end
+
+      private
+
+      def candidate_resources
+        associated_class.all
+      end
+
+      def display_candidate_resource(resource)
+        associated_dashboard.display_resource(resource)
       end
     end
   end
